60ec567da9e9d0f04172ffb2f8beb76d0ff13943
qwasdfzxcv <110401568+qwasdfzxcv@users.noreply.github.com>
2023-07-03 09:04:10 +0000

    vim

---
+++ java/aa.java
@@ -0,0 +1,9 @@
+package java;
+
+class aa{
+    public static void main(String[] args) {
+        System.out.println("13856");
+        System.out.println("13856");
+        System.out.println("13856");
+    }
+}
